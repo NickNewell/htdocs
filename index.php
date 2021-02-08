99eab11cce4f5f2e6f7baa31074063e7bd22ffee
Nick <Nicholas.a.newell@gmail.com>
2021-02-08 16:00:27 -0600

    Refining of JUNK

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  include 'templates\header.php'
+?>
+
+
+    <!-- Header -->
+    <header class="masthead bg-primary text-center">
+      <div class="container">
+        <img class="img-fluid mb-5 d-block mx-auto" src="img/medallionSimple.png" alt="">
+        <h1 class="text-uppercase mb-0">City of Manvel</h1>
+        <hr class="star-light">
+        <h2 class="font-weight-light mb-0">This is where we will be handling things internally!</h2>
+      </div>
+    </header>
+
+    
+
+    
+
+<?php
+  include 'templates\footer.php'
+?>
